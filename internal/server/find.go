5dae07786760bde9fad9adb5720fa136f9888fb1
Daniil Turaev <adef96@mail.ru>
2023-01-15 22:43:01 +0300

    Add CORS and a new word

--- internal/server/find.go
+++ internal/server/find.go
@@ -10,6 +10,7 @@ type findRequest struct {
 	Ignored string `json:"ignored"`
 	Guessed string `json:"guessed"`
 	Pattern string `json:"pattern"`
+	Limit   uint32 `json:"limit"`
 }
 
 type findResponse struct {
@@ -17,6 +18,16 @@ type findResponse struct {
 }
 
 func (s *Server) findHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte{})
+
+		return
+	}
+
 	if r.Body == http.NoBody {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("no body"))
@@ -54,6 +65,9 @@ func (s *Server) findHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	res := s.t.Find(req.Pattern, ignored, guessed)
+	if req.Limit != 0 && len(res) > int(req.Limit) {
+		res = res[:req.Limit]
+	}
 
 	bb, err = json.Marshal(res)
 	if err != nil {
